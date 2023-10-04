2c5fe10034feca89b2ead9c89d0e1ce6cbfa0bfc
Richard Li <r.li@hotmail.de>
2023-10-04 19:53:18 +0200

    adding builder annotation

--- src/main/java/com/example/immomio_coding/entities/Album.java
+++ src/main/java/com/example/immomio_coding/entities/Album.java
@@ -2,6 +2,7 @@ package com.example.immomio_coding.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Album {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
