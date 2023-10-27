78f01f09e0f72906bcd1e127d57607029364263b
Richard Li <r.li@hotmail.de>
2023-10-27 16:16:20 +0200

    adding tests for spotifyAPI

--- src/main/java/com/example/immomio_coding/ImmomioCodingApplication.java
+++ src/main/java/com/example/immomio_coding/ImmomioCodingApplication.java
@@ -2,14 +2,9 @@ package com.example.immomio_coding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableScheduling
 @EnableTransactionManagement
 public class ImmomioCodingApplication {
 
